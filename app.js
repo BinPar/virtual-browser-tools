6d94b5457106f40de05297cad746a7c68d4c7956
drnachio <nacho@binpar.com>
2017-01-15 12:47:22 +0100

    fixes #1

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var nightmare = Nightmare({ show: false });
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+
+let logOptions = {
+		noLog:1,
+		errors:2,
+		info:3,
+		verbose:4
+};
+
+let logMode = logOptions[process.env.LOG] || logOptions.info;
+
+if(logMode >= logOptions.noLog) console.log("Log mode:" + logMode);
+
 nightmare.viewport(1920, 1080);
 
 app.use(bodyParser.json());
@@ -19,36 +31,50 @@ var DEFAULT_SCREENSHOT_CLIP_OPTIONS = {
 var getPicture = function(url, options) {
 	options = options || {};
 	var res = nightmare.goto(url + '?v=' + new Date().getTime());
-	console.log('[getPicture] after goto');
+	if(logMode >= logOptions.info) console.log('[getPicture] after goto');
 	if(options.selector) {
 		res = res.wait(options.selector);
-		console.log('[getPicture] after wait selector');
+		if(logMode >= logOptions.info) console.log('[getPicture] after wait selector');
 	} else {
 		res = res.wait(options.waitTimeout || 200);
-		console.log('[getPicture] after wait timeout');
+		if(logMode >= logOptions.info) console.log('[getPicture] after wait timeout');
 	}
 	res = res.evaluate(function() {
 		document.body.style.transformOrigin = "0 0";
 		document.body.style.transform = "scale(" + (1 / devicePixelRatio) + ")";
 	});
-	console.log('[getPicture] after evaluate');
+	if(logMode >= logOptions.info)console.log('[getPicture] after evaluate');
 	if(options.screenshotOptions) {
 		Object.keys(options.screenshotOptions).forEach((key)=>{
 			options.screenshotOptions[key] = options.screenshotOptions[key] / devicePixelRatio;
 		});
 		options.screenshotOptions = Object.assign({}, DEFAULT_SCREENSHOT_CLIP_OPTIONS, options.screenshotOptions);
-		console.log('[getPicture] screenOptions');
+		if(logMode >= logOptions.info) console.log('[getPicture] screenOptions');
 	}
 	return res.screenshot(null, options.screenshotOptions);
 };
 
+var getPDF = function(url, options) {
+	options = options || {};
+	var res = nightmare.goto(url);
+	if(logMode >= logOptions.info) console.log('[getPDF] after goto');
+	if(options.selector) {
+		res = res.wait(options.selector);
+		if(logMode >= logOptions.info) console.log('[getPDF] after wait selector');
+	} else {
+		res = res.wait(options.waitTimeout || 200);
+		if(logMode >= logOptions.info) console.log('[getPDF] after wait timeout');
+	}
+	return res.pdf(null, options.pdfOptions || {});
+};
+
 app.post('/imageFromUrl', function (req, res) {
 	var body = req.body;
-	console.log('OPTIONS: ', body);
+	if(logMode >= logOptions.info) console.log('OPTIONS: ', body);
 	if(body.url) {
 		try {
 			if (!devicePixelRatio) {
-				console.log('NO DevicePixelRatio. Calculating...');
+				if(logMode >= logOptions.info) console.log('NO DevicePixelRatio. Calculating...');
 				nightmare
 					.goto(body.url)
 					.evaluate(function () {
@@ -56,35 +82,59 @@ app.post('/imageFromUrl', function (req, res) {
 					})
 					.then((value) => {
 						devicePixelRatio = value;
-						console.log('DevicePixelRatio: ' + value);
+						if(logMode >= logOptions.info) console.log('DevicePixelRatio: ' + value);
 						getPicture(body.url, body.options).then((img) => {
-							console.log('IMG:', img);
+							if(logMode >= logOptions.verbose) console.log('IMG:', img);
 							res.end(img);
 						}).catch(function (error) {
-							console.log('Capture failed:', error);
+							if(logMode >= logOptions.verbose) console.log('Capture failed:', error);
 							res.end('Capture failed:' + error);
 						});
 					})
 					.catch((err) => {
-						console.log('ERR: ', err);
+						if(logMode >= logOptions.error) console.log('ERR: ', err);
 						res.end('Error trying to calculate device pixel ratio');
 					});
 			} else {
 				getPicture(body.url, body.options).then((value) => {
-					console.log('IMG:', value);
+					if(logMode >= logOptions.verbose) console.log('IMG:', value);
 					res.end(value);
 				}).catch(function (error) {
-					console.log('Capture failed:', error);
+					if(logMode >= logOptions.error) console.log('Capture failed:', error);
 					res.end('Capture failed:' + error);
 				});
 			}
 		}
 		catch(err) {
-			console.log('ERR:', err);
+			if(logMode >= logOptions.error) console.log('ERR:', err);
 			res.end('Server error');
 		}
 	} else {
-		console.log('No URL. Exit');
+		if(logMode >= logOptions.error) console.log('No URL. Exit');
+		res.end('No URL. Exit');
+	}
+});
+
+
+app.post('/PDFFromUrl', function (req, res) {
+	var body = req.body;
+	if(logMode >= logOptions.info) console.log('OPTIONS: ', body);
+	if(body.url) {
+		try {
+				getPDF(body.url, body.options).then((value) => {
+					if(logMode >= logOptions.verbose) console.log('PDF:', value);
+					res.end(value);
+				}).catch(function (error) {
+					if(logMode >= logOptions.error) console.log('Capture failed:', error);
+					res.end('Capture failed:' + error);
+				});
+		}
+		catch(err) {
+			if(logMode >= logOptions.error) console.log('ERR:', err);
+			res.end('Server error');
+		}
+	} else {
+		if(logMode >= logOptions.error) console.log('No URL. Exit');
 		res.end('No URL. Exit');
 	}
 });
@@ -110,8 +160,7 @@ var port = normalizePort(process.env.PORT || '3003');
 if(process.env.IS_PROD){
 	port = normalizePort('80');
 }
-
+console.log(port);
 var server = app.listen(port, function () {
 	console.log('Example app listening on port ' + port)
 });
-
